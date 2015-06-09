105982ae5fb139cb125d56cb5eea542d238a26a2
trains629 <trains629@live.com>
2015-06-09 17:35:49 +0800

    修正输出标签

--- public/js/client.js
+++ public/js/client.js
@@ -1,17 +1,17 @@
 //client.js
 
-var info;
+var infor;
 
 window.onload = function () {
-  info = document.querySelector("#info");
+  infor = document.querySelector("#infor");
 }
 
 function player(id) {
   // 那需要一个处理ajax的函数库
   console.log(id);
   $.get("cmd?id="+id,function (data) {
-    console.log(data);
-    if(!info)return;
-    info.innerHTML = JSON.stringify(data);
+    console.log(data,infor);
+    if(!infor)return;
+    infor.innerHTML = JSON.stringify(data);
   })
 }
